1cbbb1c7c928bae7400c81cea9936813c42d0b01
Purvesh Makode <makodepurvesh@gmail.com>
2019-09-01 12:33:42 +0530

    Initial Commit

---
+++ Assets/Scenes/Scripts/modelController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class modelController : MonoBehaviour
+{
+
+    public GameObject medeaModel;
+    private Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = medeaModel.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void changeAnim(string trigger) {
+       // Debug.Log("PRESSED");
+        anim.SetTrigger(trigger);
+    }
+}
